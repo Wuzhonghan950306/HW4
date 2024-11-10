bba48830e9842db44e44b8871e5dd19bb8061f10
Wuzhonghan950306 <king10917wu@gmail.com>
2024-11-10 17:10:09 +0800

    4

---
+++ p.25/Project1/Project1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+    int x = 100;
+    int y = addbyone(x);
+    printf("x=%d\n", x);
+    system("pause");
+}
+
+int addbyone(int& xref)
+{
+    xref++;
+    printf("xref=%d\n", xref);
+    return xref;
+}
